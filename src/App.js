f618555f1adf1d3647a11853c77d30cad2429d8f
Fernanda Monroe <inandamonroe@gmail.com>
2024-02-28 15:48:56 -0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import AdminDashboard from "./pages/admin-dashboard/AdminDashboard";
 
 function App() {
-  return (
-    <div>
-      <h1></h1>
-    </div>
-  );
+    const navigate = useNavigate()
+    useEffect(() => {
+        navigate('/admin-dashboard/main')
+    }, [])
+    return (
+        <div className=" h-[100vh] w-full">
+            <Routes>
+                <Route path="/admin-dashboard/:category" element={<AdminDashboard />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
